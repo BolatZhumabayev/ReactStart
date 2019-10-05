937e6568e21f49a11c771934ccdcf3737a9189bb
Bulat Zhumabayev <bolat.zh@gmail.com>
2019-10-05 23:00:22 +0600

    add form, validate form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent'
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -49,7 +50,8 @@ class Main extends Component {
               <Route path="/home" component = {HomePage} />
               <Route exact path="/menu" component={ () => <Menu dishes={this.state.dishes}/> } />
               <Route path='/menu/:dishId' component={DishWithId} />
-              <Route exact path='/contactus' component={Contact} />} />
+              <Route path='/contactus' component={Contact} />} />
+              <Route path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
               <Redirect to="/home" />
             </Switch>
             <Footer />
