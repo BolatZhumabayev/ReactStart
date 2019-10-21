7b56fc8d5a663b48335e4e4dd1700f61c6d0606a
Bulat Zhumabayev <bolat.zh@gmail.com>
2019-10-22 03:38:44 +0600

    Fetch Post Comment2

--- src/redux/promotions.js
+++ src/redux/promotions.js
@@ -7,9 +7,9 @@ export const Promotions = (state =
         case ActionTypes.ADD_PROMOS:
             return {...state, isLoading: false, errMess: null, promotions: action.payload};
         case ActionTypes.PROMOS_LOADING:
-            return {... state, isLoading: true, errMess: null, promotions: []};
+            return {...state, isLoading: true, errMess: null, promotions:[]};
         case ActionTypes.PROMOS_FAILED:
-        return {... state, isLoading: false, errMess: action.payload};
+        return {...state, isLoading: false, errMess: action.payload};
         default:
           return state;
       }
